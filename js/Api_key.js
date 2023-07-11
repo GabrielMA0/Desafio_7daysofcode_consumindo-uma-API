13def78387eab5efe5c2ddbf92497eea8769db86
Gabriel Moreira Araújo <moreira.araujo99@gmail.com>
2023-07-10 21:07:44 -0300

    projeto finalizado.

---
+++ js/Api_key.js
@@ -0,0 +1,1 @@
+export const apiKey = "ed463b9448938ef7038f874cf62376fb";
--- js/script.js
+++ js/script.js
@@ -1,46 +1,63 @@
+import { apiKey } from "./Api_key.js";
+
 const moviesContainer = document.querySelector(".movies");
 const inputSearch = document.querySelector("#field-search");
 const inputCheckbox = document.querySelector("#input-checkbox");
+const searchButton = document.querySelector("#search-btn");
 
-const movies = [
-    {
-        image: "https://img.elo7.com.br/product/original/3FBA809/big-poster-filme-batman-2022-90x60-cm-lo002-poster-batman.jpg",
-        title: "Batman",
-        rating: 9.2,
-        year: 2022,
-        description:
-            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-        isFavorited: false,
-    },
-    {
-        image: "https://upload.wikimedia.org/wikipedia/pt/thumb/9/9b/Avengers_Endgame.jpg/250px-Avengers_Endgame.jpg",
-        title: "Avengers",
-        rating: 8.2,
-        year: 2019,
-        description:
-            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-        isFavorited: false,
-    },
-    {
-        image: "https://upload.wikimedia.org/wikipedia/en/1/17/Doctor_Strange_in_the_Multiverse_of_Madness_poster.jpg",
-        title: "Doctor Strange",
-        rating: 7.2,
-        year: 2022,
-        description:
-            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
-        isFavorited: true,
-    },
-];
+let popularMovies = [];
 
 window.onload = function () {
     inputSearch.focus();
-    movies.forEach((movie) => renderMovie(movie));
+    getPopularMovies();
 };
 
+async function getPopularMovies() {
+    const url = `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=en-US&page=1`;
+
+    const responses = await fetch(url);
+
+    const data = await responses.json();
+
+    popularMovies = data.results;
+
+    popularMovies.forEach((movies) => renderMovie(movies));
+}
+
+searchButton.addEventListener("click", async function () {
+    let searchInputValue = inputSearch.value;
+
+    const urlSearch = `https://api.themoviedb.org/3/search/movie?query=${searchInputValue}&api_key=${apiKey}`;
+
+    if (searchInputValue === "") {
+        alert("Preencha o campo!");
+    } else {
+        moviesContainer.innerText = "";
+
+        const responsesSearch = await fetch(urlSearch);
+
+        const dataSearch = await responsesSearch.json();
+
+        dataSearch.results.forEach((movieSearch) => renderMovie(movieSearch));
+    }
+});
+
+function checkFavoriteMovie(id) {
+    let favoriteMoviesJSON =
+        JSON.parse(localStorage.getItem("favoriteMovies")) || [];
+    return favoriteMoviesJSON.some((m) => m.id === id);
+}
+
 function renderMovie(movie) {
-    const { title, image, rating, year, description, isFavorited } = movie;
+    const { title, poster_path, vote_average, release_date, overview, id } =
+        movie;
+
+    let isFavorited = checkFavoriteMovie(id);
+    const imgApi = `https://image.tmdb.org/t/p/w500${poster_path}`;
+    const year = release_date.split("-")[0];
 
     const movieElement = document.createElement("div");
+    movieElement.setAttribute("data-id", id);
     movieElement.classList.add("movie");
     moviesContainer.appendChild(movieElement);
 
@@ -50,7 +67,7 @@ function renderMovie(movie) {
 
     const movieImage = document.createElement("img");
     movieImage.classList.add("img-movie");
-    movieImage.src = image;
+    movieImage.src = imgApi;
     movieImage.alt = `${title} Poster`;
     movieInfo.appendChild(movieImage);
 
@@ -75,12 +92,13 @@ function renderMovie(movie) {
     const starImage = document.createElement("img");
     starImage.src = "./assets/star.svg";
     starImage.alt = "Star";
+    starImage.title = "Avaliação";
     btnStar.appendChild(starImage);
     ratingContainer.appendChild(btnStar);
 
     const textRating = document.createElement("span");
     textRating.classList.add("text-rating");
-    textRating.textContent = rating;
+    textRating.textContent = vote_average;
     ratingContainer.appendChild(textRating);
 
     const btnFavorite = document.createElement("button");
@@ -91,13 +109,77 @@ function renderMovie(movie) {
         ? "./assets/heart-fill.svg"
         : "./assets/heart.svg";
     favoriteImage.alt = "Heart";
+    favoriteImage.title = "Favoritar";
+    favoriteImage.classList.add("favorite-image");
     movieBtns.appendChild(btnFavorite);
 
     const movieDescriptionContainer = document.createElement("div");
     movieDescriptionContainer.classList.add("description");
     const movieDescription = document.createElement("span");
     movieDescription.classList.add("movie-description");
-    movieDescription.textContent = description;
+    movieDescription.textContent = overview;
     movieInfo.appendChild(movieDescriptionContainer);
     movieDescriptionContainer.appendChild(movieDescription);
+
+    btnFavorite.addEventListener("click", function () {
+        saveFavoriteMovie(movie, isFavorited);
+    });
 }
+
+function saveFavoriteMovie(movie, isFavorited) {
+    isFavorited = !isFavorited;
+    let movieDiv = document.querySelector(`[data-id="${movie.id}"]`);
+
+    const favoriteImage = movieDiv.querySelector(".favorite-image"); // primeiro filho
+
+    if (isFavorited) {
+        let getFavoriteMoviesJSON =
+            JSON.parse(localStorage.getItem("favoriteMovies")) || [];
+        getFavoriteMoviesJSON.push(movie);
+        const favoriteMoviesJSON = JSON.stringify(getFavoriteMoviesJSON);
+        localStorage.setItem("favoriteMovies", favoriteMoviesJSON);
+        favoriteImage.src = "./assets/heart-fill.svg";
+    } else {
+        favoriteImage.src = "./assets/heart.svg";
+        removeFavoriteMovie(movie);
+    }
+}
+
+function removeFavoriteMovie(movie) {
+    let getFavoriteMoviesJSON = JSON.parse(
+        localStorage.getItem("favoriteMovies")
+    );
+    const indexMovie = getFavoriteMoviesJSON.findIndex(
+        (m) => m.id === movie.id
+    );
+    if (indexMovie !== -1) {
+        getFavoriteMoviesJSON.splice(indexMovie, 1);
+        const favoriteMoviesJSON = JSON.stringify(getFavoriteMoviesJSON);
+        localStorage.setItem("favoriteMovies", favoriteMoviesJSON);
+        if (inputCheckbox.checked) {
+            let movieElement = document.querySelector(
+                `[data-id="${movie.id}"]`
+            );
+            movieElement.remove();
+        }
+    }
+}
+
+inputCheckbox.addEventListener("change", function () {
+    if (inputCheckbox.checked) {
+        moviesContainer.innerText = "";
+        let favoriteMoviesJSON = JSON.parse(
+            localStorage.getItem("favoriteMovies")
+        );
+        favoriteMoviesJSON.forEach((movie) => renderMovie(movie));
+    } else {
+        moviesContainer.innerText = "";
+        getPopularMovies();
+    }
+});
+
+document.addEventListener("keypress", function (e) {
+    if (e.key === "Enter") {
+        searchButton.click();
+    }
+});
